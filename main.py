40a6b3150ffcd6449df83545a251667c67689519
DawidKolo <dawid_kolodziej@o2.pl>
2023-08-10 07:37:33 +0200

    polish

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ users = parsed_json['users']
 for x in users:
     pdf = FPDF()
     pdf.add_page()
+    pdf.add_font('Arial', '', 'c:\windows\Fonts\Arial.ttf', uni=True)
     pdf.set_font('Arial', size = 15)
     pdf.cell(200, 10, txt= f"{x['name']} {x['surname']}",ln = 1, align= 'L')
-    pdf.cell(200, 10, txt= "Wygrales "+ f"{x['reward']}", ln = 1, align= 'L')
+    pdf.cell(200, 10, txt= "Wygrałeś "+ f"{x['reward']}", ln = 1, align= 'L')
 
     pdf.output(f"{x['name']}.pdf")
 
