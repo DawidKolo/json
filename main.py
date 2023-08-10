e991d9a3cf4cc5f07d211834606902a5d3eac4ca
DawidKolo <dawid_kolodziej@o2.pl>
2023-08-10 07:45:18 +0200

    reformat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import json
 from fpdf import FPDF
 
-
-
-
 with open('data.json') as user_file:
     file_contents = user_file.read()
 
@@ -14,13 +11,9 @@ users = parsed_json['users']
 for x in users:
     pdf = FPDF()
     pdf.add_page()
-    pdf.add_font('Arial', '', 'c:\windows\Fonts\Arial.ttf', uni=True)
-    pdf.set_font('Arial', size = 15)
-    pdf.cell(200, 10, txt= f"{x['name']} {x['surname']}",ln = 1, align= 'L')
-    pdf.cell(200, 10, txt= "Wygrałeś "+ f"{x['reward']}", ln = 1, align= 'L')
+    pdf.add_font('Arial', '', 'c:\\windows\\Fonts\\Arial.ttf', uni=True)
+    pdf.set_font('Arial', size=15)
+    pdf.cell(200, 10, txt=f"{x['name']} {x['surname']}", ln=1, align='L')
+    pdf.cell(200, 10, txt="Wygrałeś " + f"{x['reward']}", ln=1, align='L')
 
     pdf.output(f"{x['name']}.pdf")
-
-
-
-
