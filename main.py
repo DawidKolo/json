61166192116d84bd81c5375b742ba343d8d070b0
DawidKolo <dawid_kolodziej@o2.pl>
2023-08-10 14:36:37 +0200

    reformat

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from flask import *
 import json
 from fpdf import FPDF
@@ -23,22 +21,6 @@ def create_pdf():
 
     return jsonify(users)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-# with open('json.json') as user_file:
-#     file_contents = user_file.read()
-#
-# json = json.loads(file_contents)
-#print(json)
-#users = parsed_json['users']
-
-
-#for x in users:
-    # pdf = FPDF()
-    # pdf.add_page()
-    # pdf.add_font('Arial', '', 'c:\\windows\\Fonts\\Arial.ttf', uni=True)
-    # pdf.set_font('Arial', size=15)
-    # pdf.cell(200, 10, txt=f"{x['name']} {x['surname']}", ln=1, align='L')
-    # pdf.cell(200, 10, txt="Wygrałeś " + f"{x['reward']}", ln=1, align='L')
-    #
-    # pdf.output(f"{x['name']}.pdf")
